ac745f1ad61fe77f0be8e413dfba94ef75ad2978
Dipen Shah <dipens@stridely.local>
2020-02-06 14:57:41 +0530

    Sample CRUD Operations reactjs

---
+++ src/webparts/sampleCrudReactjs/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISampleCrudReactjsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SampleCrudReactjsWebPartStrings' {
+  const strings: ISampleCrudReactjsWebPartStrings;
+  export = strings;
+}
