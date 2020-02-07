07a56e993121d31f04753e0cac739d5ee28f460a
Dipen Shah <dipens@stridely.local>
2020-02-07 16:58:52 +0530

    Added validation on PropertyPane

--- src/webparts/sampleCrudReactjs/SampleCrudReactjsWebPart.ts
+++ src/webparts/sampleCrudReactjs/SampleCrudReactjsWebPart.ts
@@ -7,11 +7,11 @@ import {
   PropertyPaneTextField,
   PropertyPaneLabel
 } from '@microsoft/sp-webpart-base';
-
+import { SPHttpClientResponse,SPHttpClient } from "@microsoft/sp-http";
 import * as strings from 'SampleCrudReactjsWebPartStrings';
 import SampleCrudReactjs from './components/SampleCrudReactjs';
 import { ISampleCrudReactjsProps } from './components/ISampleCrudReactjsProps';
-
+import { Environment,EnvironmentType } from "@microsoft/sp-core-library";
 export interface ISampleCrudReactjsWebPartProps {
   description: string;
   ListName:string;
@@ -41,6 +41,45 @@ export default class SampleCrudReactjsWebPart extends BaseClientSideWebPart<ISam
     return Version.parse('1.0');
   }
 
+  private ValidateListName(value:string):Promise<string> {
+
+    return new Promise<string>((
+      resolve: (validationErrorMessage: string) => void, 
+      reject: (error: any) => void): void => 
+      {
+
+        if(Environment.type != EnvironmentType.SharePoint){
+          resolve("Please connect to SharePoint enviornment.");
+          return;
+        }
+
+
+        if(value === null || value.length === 0){
+        resolve("Provide the list name");
+        return;
+      }
+
+    // Check List exist or not
+    this.context.spHttpClient.get(
+      `${this.context.pageContext.site.absoluteUrl}/_api/web/lists/getytitle('${escape(value)}')?$select=Id`,
+      SPHttpClient.configurations.v1).then((response:SPHttpClientResponse):void=>{
+      if(response.ok){
+        resolve('');
+      }
+      else if(response.status === 404){
+        resolve(`List ${escape(value)} does not exist in site`);
+      }
+      else{
+        resolve(`Error: ${response.statusText}. Please try again`);
+        return;
+      }
+      })
+      .catch((error:any):void=>{
+        resolve(error);
+      });
+    });
+  }
+
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
@@ -59,7 +98,8 @@ export default class SampleCrudReactjsWebPart extends BaseClientSideWebPart<ISam
                   text:"List Name"
                 }),
                 PropertyPaneTextField('ListName',{
-                  label:strings.ListNameFieldLabel
+                  label:strings.ListNameFieldLabel,
+                  onGetErrorMessage:this.ValidateListName.bind(this)
                 })
               ]
             }
